ea3a3c3c512f2bcbf8c3df769a5d88639b3ed1de
Joao Lima <joaoag108@gmail.com>
2026-02-24 13:05:55 -0300

    Criado o projeto do assistente virtual de IA Jarvis

---
+++ src/main/java/com/jaugusto/AssistenteJarvisAI/AssistenteJarvisAiApplication.java
@@ -0,0 +1,13 @@
+package com.jaugusto.AssistenteJarvisAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistenteJarvisAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistenteJarvisAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaugusto/AssistenteJarvisAI/AssistenteJarvisAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaugusto.AssistenteJarvisAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistenteJarvisAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
